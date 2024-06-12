c12490af23793c871e905489dba3e9fd920c8fc7
ostephan <olivier.stephan@softeam.fr>
2024-06-12 13:53:28 +0200

    Ajout Analytics vercel

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,13 @@
 import { StrictMode } from 'react';
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import { Analytics } from '@vercel/analytics/react';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <StrictMode><Component {...pageProps} /></StrictMode>;
+  return <StrictMode>
+      <Component {...pageProps} />
+      <Analytics />
+    </StrictMode>;
 }
 
 export default MyApp;
